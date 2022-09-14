3c3cb5234998806e3e2988cc199d9e1c5008c08e
UWillno <uwillno@outlook.com>
2022-09-14 18:53:55 +0800

    +vue

---
+++ src/main/vue/src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="login">
+		<h1>登录</h1>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	#login{
+		width: 100%;
+		height: 100%;
+		display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image:url('https://www.sf-express.com/uploads/speedy_express_edc602d35a.jpg');
+  }
+</style>
