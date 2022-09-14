6816fc840e572ee76b667c7cfd78a35fa1b0f6a4
UWillno <uwillno@outlook.com>
2022-09-14 16:05:09 +0800

    登录加密

--- src/main/java/cn/whjg/student/controller/ManagerController.java
+++ src/main/java/cn/whjg/student/controller/ManagerController.java
@@ -1,9 +1,13 @@
 package cn.whjg.student.controller;
 
 import cn.whjg.student.common.R;
+import cn.whjg.student.entity.Manager;
 import cn.whjg.student.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +21,10 @@ public class ManagerController {
         return R.OK(managerService.login(username, password));
     }
 
+    @PostMapping("/manager/register")
+    public R register(@RequestBody Manager manager) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        manager.setPassword(passwordEncoder.encode(manager.getPassword()));
+        return R.OK(managerService.save(manager));
+    }
 }
